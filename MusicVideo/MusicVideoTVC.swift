5cb42a6a8ddee2addc91d15de1338ca02a1fd7c4
Charles Swafford <edswafford@mchsi.com>
2016-07-15 06:38:09 -0500

    Completed part 13

--- MusicVideo/MusicVideoTVC.swift
+++ MusicVideo/MusicVideoTVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class MusicVideoTVC: UITableViewController {
-
+    
     var videos = [Videos]()
     
     override func viewDidLoad() {
@@ -19,126 +19,145 @@ class MusicVideoTVC: UITableViewController {
         
         reachabilityStatusChanged()
         
+    }
+    
+    func didLoadData(videos: [Videos]) {
+        print(reachabilityStatus)
+        
+        self.videos = videos
+        
+        for (index, item) in videos.enumerate() {
+            print("\(index) name = \(item.vName)")
+        }
+        tableView.reloadData()
+    }
+    
+    
+    func runAPI() {
         
         // Call API
         let api = APIManager()
         api.loadData("http://itunes.apple.com/us/rss/topmusicvideos/limit=50/json", completion: didLoadData)
     }
     
-    func didLoadData(videos: [Videos]) {
-        
-        print(reachabilityStatus)
-        
-        self.videos = videos
-        
-        
-        for (index, item) in videos.enumerate() {
-            print("\(index) name = \(item.vName)")
-        }
-        
-        tableView.reloadData()
-        /*
-         ******  Alert Example
-         */
-        
-        //        let alert = UIAlertController(title: (result), message: nil, preferredStyle: .Alert)
-        //        let okAction = UIAlertAction(title: "OK", style: .Default) { action -> Void in
-        //            // do something if you want
-        //        }
-        //        alert.addAction(okAction)
-        //        self.presentViewController(alert, animated: true, completion: nil)
-    }
-    
-    
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
     }
     
     func reachabilityStatusChanged() {
+        
         switch reachabilityStatus {
         case NOACCESS:
             view.backgroundColor = UIColor.redColor()
-           // displayLabel.text = "No Internet"
             
-        case WIFI:
-            view.backgroundColor = UIColor.greenColor()
-            //displayLabel.text = "Reachable with WIFI"
+            // It is possible the view has not loaded when we try to dislay the alert which will give this warning
+            //
+            //  Presenting view controllers on detached view controllers is discouraged
+            //
+            // The fix is to run this in the Main Queue -- NOT Sure Why
+            //
+            dispatch_async(dispatch_get_main_queue(), {
+                
+                let alert = UIAlertController(title: "No Internet Acess", message: "Please make sure you are connected to the Internet", preferredStyle: .Alert)
+                
+                let cancelAction = UIAlertAction(title: "Cancel", style: .Default, handler: { (action) in
+                    print("Cancel")
+                })
+                let deleteAction = UIAlertAction(title: "Delete", style: .Destructive, handler: { (action) in
+                    print("Delete")
+                })
+                let okAction = UIAlertAction(title: "Ok", style: .Default, handler: { (action) in
+                    print("OK")
+                })
+                
+                alert.addAction(okAction)
+                alert.addAction(cancelAction)
+                alert.addAction(deleteAction)
+                
+                self.presentViewController(alert, animated: true, completion: nil)
+            })
             
-        case WWAN:
-            view.backgroundColor = UIColor.yellowColor()
-            //displayLabel.text = "Reachable with Cellular"
+            //case WWAN:
+            //    view.backgroundColor = UIColor.yellowColor()
             
         default:
-            return
+            view.backgroundColor = UIColor.greenColor()
+            if videos.count > 0 {
+                print("Do not refresh API")
+            }
+            else {
+                runAPI()
+            }
+            
         }
     }
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return videos.count
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
-
+        
         let video = videos[indexPath.row]
         cell.textLabel?.text = "\(indexPath.row + 1)"
         cell.detailTextLabel?.text = video.vName
-
+        
         return cell
     }
     
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+     if editingStyle == .Delete {
+     // Delete the row from the data source
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+     } else if editingStyle == .Insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
