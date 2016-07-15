e9a2e772cdd5273dca478d3d32a2d877473d75af
Charles Swafford <edswafford@mchsi.com>
2016-07-15 10:30:07 -0500

    completed part 15

--- MusicVideo/MusicVideoTableViewCell.swift
+++ MusicVideo/MusicVideoTableViewCell.swift
@@ -25,6 +25,43 @@ class MusicVideoTableViewCell: UITableViewCell {
     func updateCell() {
         musicTitle.text = video?.vName
         rank.text = "\(video!.vRank)"
-        musicImage.image = UIImage(named: "imageNotAvailable")
+        //musicImage.image = UIImage(named: "imageNotAvailable")
+        
+        if video!.vImageData != nil {
+            print("Get data from array ...")
+            musicImage.image = UIImage(data: video!.vImageData!)
+        }
+        else {
+            getVideoImage(video!, imageView: musicImage)
+        }
+    }
+    
+    
+    func getVideoImage(video: Videos, imageView: UIImageView) {
+        // Background Thread
+        // DISPATCH_QUEUE_PRIORITY_HIGH Items dispatched to the queue will run at high
+        // priority, i.e. the queue will schedule for execution before any default
+        // priority or low priority queue.
+        
+        // DISPATCH_QUEUE_PRIORITY_LOW Items dispatched to the queue will run at the
+        // default priority, i.e. the queue will schedule for execution after all high
+        // priority queues have been scheduled, but before low priority queue.
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            let data = NSData(contentsOfURL: NSURL(string: video.vImageUrl)!)
+            
+            var video = video
+            var image: UIImage?
+            if data != nil {
+                video.vImageData = data
+                image = UIImage(data: data!)
+            }
+            
+            // move back to Main Queue
+            dispatch_async(dispatch_get_main_queue(), { 
+                imageView.image = image
+            })
+            
+        }
     }
 }
