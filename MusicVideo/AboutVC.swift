5e20ba01264e143c1c8c80e92f7337b21b73d37a
Charles Swafford <edswafford@mchsi.com>
2016-07-21 11:45:48 -0500

    add about page

---
+++ MusicVideo/AboutVC.swift
@@ -0,0 +1,18 @@
+//
+//  AboutVC.swift
+//  MusicVideo
+//
+//  Created by Charles Swafford on 7/21/16.
+//  Copyright © 2016 learn IOS 9. All rights reserved.
+//
+
+import UIKit
+
+class AboutVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "About"
+    }
+}
